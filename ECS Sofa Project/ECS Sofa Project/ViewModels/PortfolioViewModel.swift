31be19ddb0f19992018011013953a54ed7373ac3
leodan98 <118185699+Leonarmidler@users.noreply.github.com>
2023-01-19 14:35:45 +0100

    added api request from github

--- ECS Sofa Project/ECS Sofa Project/ViewModels/PortfolioViewModel.swift
+++ ECS Sofa Project/ECS Sofa Project/ViewModels/PortfolioViewModel.swift
@@ -9,15 +9,38 @@ import Foundation
 import PhotosUI
 import SwiftUI
 
+@MainActor
 final class PortfolioViewModel: ObservableObject {
+    // API VARIABLES
+    @Published var repositories: [Repository] = []
+    let decoder = JSONDecoder()
+    
     @Published var isAddingProject = false
     @Published var projectArray = [
         ProjectModel(title: "Title", image: UIImage(named: "Project")),
         ProjectModel(title: "Title", image: UIImage(named: "Project")),
         ProjectModel(title: "Title", image: UIImage(named: "Project"))
     ]
-
+    
     func addProject(newProject: ProjectModel) {
         projectArray.append(newProject)
+    }
+    
+    func getRepositories() async {
+        do {
+            let url = URL(string: "https://api.github.com/users/Alessandro-Cei/repos?type=all")
+            let request = URLRequest(url: url!)
+            let (data, _) = try await URLSession.shared.data(for: request)
+            
+            self.repositories = try decoder.decode([Repository].self, from: data)
+            
+            
+            for repo in repositories {
+                print(repo.name)
+            }            
+        } catch {
+            print("SONO NEL CATCH")
+            print(error.localizedDescription)
+        }
     }
 }
--- ECS Sofa Project/ECS Sofa Project/Views/Tabs/InProgressView.swift
+++ ECS Sofa Project/ECS Sofa Project/Views/Tabs/InProgressView.swift
@@ -8,8 +8,16 @@
 import SwiftUI
 
 struct InProgressView: View {
+    @ObservedObject var viewModel = PortfolioViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button {
+            Task {
+                await viewModel.getRepositories()
+            }
+        } label: {
+            Text("PRINTO")
+        }
     }
 }
 
