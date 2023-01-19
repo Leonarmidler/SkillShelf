b70da0e09e906ea8e9654753165d2099f01c3a84
Alessandro-Cei <92535553+Alessandro-Cei@users.noreply.github.com>
2023-01-19 15:38:00 +0100

    Added AddProjectModal

--- ECS Sofa Project/ECS Sofa Project/Views/Tabs/Portfolio/Items/AddProjectModal.swift
+++ ECS Sofa Project/ECS Sofa Project/Views/Tabs/Portfolio/Items/AddProjectModal.swift
@@ -5,48 +5,109 @@
 //  Created by Leonardo Daniele on 11/01/23.
 //
 
-import PhotosUI
 import SwiftUI
+import PhotosUI
 
 struct AddProjectModal: View {
-    @State private var newProject = ProjectModel(title: "", image: nil)
+    
     @ObservedObject var viewModel: PortfolioViewModel
+    @State private var newProject = ProjectModel(title: "", description: "", tags: [])
+    @State var tagList: [Tags] = [.SwiftUI, .UIKit, .CoreML, .CoreData, .PhotosUI]
+    @State var tagViews: [TagView] = []
+    @State var isSelectingTag: Bool = false
+    
+    let columns: [GridItem] = [GridItem(), GridItem(), GridItem()]
+    
     var body: some View {
         NavigationStack {
-            VStack {
-                Form {
-                    Section {
-                        TextField("Enter project title", text: $newProject.title)
+            GeometryReader { geo in
+                VStack {
+                    Form {
+                        Section("") {
+                            PickerView(newProject: $newProject)
+                                .frame(width: geo.size.width, height: geo.size.height * 0.2)
+                        }
+                        .listRowBackground(Color.clear)
+                        Section("Title") {
+                            TextField("Enter project title", text: $newProject.title)
+                        }
+                        Section("Description") {
+                            TextField("Enter project description", text: $newProject.description)
+                        }
+                        Section("Tags") {
+                            VStack{
+                                Button(action: {
+                                    isSelectingTag.toggle()
+                                }, label: {
+                                    HStack{
+                                        Image(systemName: "plus.circle.fill")
+                                            .foregroundColor(Color(UIColor.systemGreen))
+                                        Text("Add a tag")
+                                            .foregroundColor(Color(UIColor.label))
+                                    }
+                                })
+                                .offset(x: 0, y: 3)
+                                LazyVGrid(columns: columns){
+                                    ForEach(tagViews){ tagView in
+                                        tagView
+                                    }
+                                }
+                            }
+                            .sheet(isPresented: $isSelectingTag){
+                                TagSelectionView(isSelectingTag: $isSelectingTag, tagViews: $tagViews, viewModel: viewModel)
+                            }
+                        }
+                        Spacer()
+                            .listRowBackground(Color.clear)
+                        Section("") {
+                            HStack{
+                              Spacer()
+                                Button(action: {
+                                    //Opens the GitHub retrieval sheet
+                                }, label: {
+                                    Text("Retrieve from GitHub")
+                                        .frame(width: geo.size.width * 0.6, height: geo.size.height * 0.07)
+                                        .background(Color.blue)
+                                        .foregroundColor(.white)
+                                        .cornerRadius(16)
+                                })
+                              Spacer()
+                            }
+                        }
+                        .listRowBackground(Color.clear)
                     }
-                    Section("Select image for your project") {
-                        PickerView(newProject: $newProject)
+                    .formStyle(.grouped)
+                }
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button {
+                            viewModel.addProject(newProject: newProject)
+                            viewModel.isAddingProject = false
+                        } label: {
+                            Text("Add")
+                        }
+                        
+                    }
+                    ToolbarItem(placement: .cancellationAction) {
+                        Button {
+                            viewModel.isAddingProject = false
+                        } label: {
+                            Text("Cancel")
+                        }
                     }
                 }
-                .formStyle(.grouped)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button {
-                        viewModel.addProject(newProject: newProject)
-                        viewModel.isAddingProject = false
-                    } label: {
-                        Text("Add")
-                    }
-                }
-                ToolbarItem(placement: .cancellationAction) {
-                    Button {
-                        viewModel.isAddingProject = false
-                    } label: {
-                        Text("Cancel")
-                    }
-                }
+                .navigationTitle("Add Project")
+                .navigationBarTitleDisplayMode(.inline)
             }
         }
     }
 }
 
-// struct AddProjectModal_Previews: PreviewProvider {
-//    static var previews: some View {
-//        AddProjectModal(isAddingProject: .constant(false))
-//    }
-// }
+struct AddProjectModal_Previews: PreviewProvider {
+    static var previews: some View {
+        AddProjectModal(viewModel: PortfolioViewModel())
+            .preferredColorScheme(.dark)
+    }
+}
+
+
