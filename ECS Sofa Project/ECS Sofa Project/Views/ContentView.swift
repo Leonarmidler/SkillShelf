268a595857dd4e0fac42886faa92f7a1c2a369a0
Serena Palumbo <serenapalumbo22@gmail.com>
2023-02-02 15:04:34 +0100

    Added onboarding
    mamma mia

--- ECS Sofa Project/ECS Sofa Project/Views/ContentView.swift
+++ ECS Sofa Project/ECS Sofa Project/Views/ContentView.swift
@@ -11,6 +11,25 @@ struct ContentView: View {
     @Environment(\.colorScheme) var colorScheme
     @StateObject private var viewModel = PortfolioViewModel()
     @StateObject private var dataController = DataController()
+    @AppStorage("isOnboarded") var isOnboarded = false
+    
+    init() {
+        if !isOnboarded {
+            var tags: [String] = []
+
+            tags.append("SwiftUI")
+            tags.append("UIKit")
+            tags.append("CoreML")
+            tags.append("CoreData")
+            tags.append("PhotosUI")
+
+            for tag in tags {
+                dataController.addTag(tag: tag)
+            }
+            
+            UserDefaults.standard.set(true, forKey: "isOnboarded")
+        }
+    }
     
     var body: some View {
         TabView {
